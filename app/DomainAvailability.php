f8aa96b11e1a0e76766c90926fd418e559874cd0
Janne Martikainen <martikainen.janne@gmail.com>
2016-07-05 23:27:26 +0300

    Bug fix on domain name check

--- app/DomainAvailability.php
+++ app/DomainAvailability.php
@@ -57,6 +57,8 @@ class DomainAvailability extends Model implements
      */
     public static function checkAvailability(array $domains) {
         
+        $ret = array();
+
         //First, let's clean the return array
         if(is_array($domains)) {
             foreach($domains as $i => $domain) {
@@ -70,6 +72,9 @@ class DomainAvailability extends Model implements
 
                 $_dashes = explode(':', $_domain);
                 $_domain = $_dashes[0];
+
+                $domain_name = false;
+                $tld = false;
 
                 $parts = explode(".", $_domain);
                 if(count($parts) == 1) {
@@ -85,13 +90,16 @@ class DomainAvailability extends Model implements
                     $tld = trim($parts[2]);
                 }  
 
-                $domains[$i] = DomainAvailability::performCheck($domain_name, $tld);
-
-                usleep(1000000);   //Sleep for a second
+                if($domain_name && $tld) {
+                    if(isset(self::$whois_servers[$tld])) {
+                        $ret[] = DomainAvailability::performCheck($domain_name, $tld);
+                        usleep(500000);   //Sleep for half of a second
+                    }
+                }
             }
         }
 
-        return $domains;
+        return $ret;
     }
 
     public static function performCheck($domainName = null, $tld = 'fi') {
@@ -103,23 +111,25 @@ class DomainAvailability extends Model implements
         ];
         
         if($domainName && $tld) {
-            $fp = fsockopen(self::$whois_servers[$tld]['server'], 43, $errstr, $errno, 10);
-            
-            fputs($fp, $domainName.'.'.$tld."\r\n");
-            
-            $rowCount = 0;  //Dummy failsafe
-            $text = '';
-            while(!feof($fp) || $rowCount <= 100)
-            {
-                $text .= fgets($fp, 4096);    
-                $rowCount++;
-            }
+            if(isset($whois_servers[$tld]['server'])) {
+                $fp = fsockopen(self::$whois_servers[$tld]['server'], 43, $errstr, $errno, 10);
+                
+                fputs($fp, $domainName.'.'.$tld."\r\n");
+                
+                $rowCount = 0;  //Dummy failsafe
+                $text = '';
+                while(!feof($fp) || $rowCount <= 100)
+                {
+                    $text .= fgets($fp, 4096);    
+                    $rowCount++;
+                }
 
-            $ret['whois'] = $text;
-            
-            //Täydennetään server-arrayta
-            if(preg_match("/".self::$whois_servers[$tld]['response']."/",$text, $matches)){
-                $ret['status'] = true;
+                $ret['whois'] = strip_tags(addslashes($text));
+                
+                //Täydennetään server-arrayta
+                if(preg_match("/".self::$whois_servers[$tld]['response']."/",$text, $matches)){
+                    $ret['status'] = true;
+                }
             }
         }
 
